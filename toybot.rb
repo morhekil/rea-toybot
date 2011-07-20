c28a5af4201c5b9649c40f2ae763315d937296ff
Oleg Ivanov <morhekil@morhekil.net>
2011-07-20 18:43:40 +0300

    Toybot initialization and error handling for UI script

--- toybot.rb
+++ toybot.rb
@@ -4,6 +4,10 @@ require 'rubygems'
 require 'bundler/setup'
 require 'lib/toybot'
 
+toybot = Toybot::Engine.new(5, 5)
 Toybot::Input.start do |cmd, args|
-  $stdout.puts "CMD: #{cmd.inspect}, ARGS: #{args.inspect}"
+  error = catch(:error) {
+    toybot.execute(cmd, args)
+  }
+  $stdout.puts "ERR: #{error}" if error.is_a?(String)
 end
