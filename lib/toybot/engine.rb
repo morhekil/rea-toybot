64d65a792d9214f031abfa228705c0fe71e1140e
Oleg Ivanov <morhekil@morhekil.net>
2011-07-20 15:19:31 +0300

    PLACE command implemented

--- lib/toybot/engine.rb
+++ lib/toybot/engine.rb
@@ -18,10 +18,25 @@ module Toybot
     # Toybot's orientation
     attr_accessor :dir
 
+    # Valid directions
+    DIR = %w{north east south west}
+
     state_machine :state, :initial => :inactive do
-      state :inactive
-      state :active
-      state :blocked
+      # Stable states
+      state :inactive, :active, :blocked
+      # Transient states
+      state :positioning, :turning_left, :turning_right, :moving
+
+      event :place_command do
+        transition any => :positioning
+      end
+      before_transition any => :positioning, :do => :validate_position
+      after_transition any => :positioning, :do => :place
+
+      event :activate do
+        transition any => :active
+      end
+
     end
 
     # Constructor takes board's width and height as it's arguments -
@@ -35,7 +50,36 @@ module Toybot
     SLIME_OUTPUT = %w{3,3,NORTH}
 
     def execute(cmd, args)
+      event_name = "#{cmd}_command".to_sym
+      throw :error, 'Invalid command' unless respond_to?(event_name)
+      send(event_name, *args)
+
       SLIME_OUTPUT.shift
+    end
+
+    # Returns an array of [posx, posy] for easy access
+    def pos
+      [posx, posy]
+    end
+
+    protected
+
+    # Executes PLACE command, updating Toybot's coordinates and direction
+    def place(transition)
+      newx, newy, newdir = transition.args
+      @posx = newx.to_i
+      @posy = newy.to_i
+      @dir = newdir
+      activate
+    end
+
+    # Validates new position given to the PLACE command
+    def validate_position(transition)
+      newx, newy, newdir = transition.args
+      valid = (0..(board_width-1)).include?(newx.to_i)
+      valid &&= (0..(board_height-1)).include?(newy.to_i)
+      valid &&= DIR.include?(newdir)
+      throw :halt unless valid
     end
 
   end
--- spec/unit/engine_spec.rb
+++ spec/unit/engine_spec.rb
@@ -8,13 +8,89 @@ describe 'Toybot Engine' do
     @toybot = Toybot::Engine.new(5, 3)
   end
 
-  it 'should remember dimensions of the board' do
-    @toybot.board_width.should == 5
-    @toybot.board_height.should == 3
+  describe 'initialization' do
+    it 'should remember dimensions of the board' do
+      @toybot.board_width.should == 5
+      @toybot.board_height.should == 3
+    end
+
+    it 'should start in inactive state' do
+      @toybot.state.should == 'inactive'
+    end
+
+    it 'should start with nil coordinates and orientations' do
+      @toybot.posx.should be_nil
+      @toybot.posy.should be_nil
+      @toybot.dir.should be_nil
+    end
   end
 
-  it 'should start in inactive state' do
-    @toybot.state.should == 'inactive'
+  describe 'commands execution' do
+    it 'should fire relevant event if defined' do
+      args = %w{one two three}
+      @toybot.should_receive(:boo_command).once.with(*args)
+      @toybot.execute('boo', args)
+    end
+
+    it 'should throw an error if the command is unknown' do
+      expect { @toybot.execute('boom', nil) }.to throw_symbol(:error)
+    end
+  end
+
+  describe 'PLACE command' do
+    def docmd(args)
+      @toybot.execute('place', args)
+      @toybot
+    end
+
+    it 'should be known to the engine' do
+      @toybot.should respond_to(:place_command)
+    end
+
+    describe 'with valid arguments' do
+      before do
+        docmd(%w{1 2 north})
+      end
+
+      it 'should set the position' do
+        @toybot.pos.should == [1, 2]
+      end
+
+      it 'should set the direction' do
+        @toybot.dir.should_not be_nil
+      end
+
+      it 'should activate Toybot' do
+        @toybot.should be_active
+      end
+    end
+
+    describe 'with invalid arguments' do
+      before do
+        docmd(%w{100 -100 inside})
+      end
+
+      it 'should stay in inactive state' do
+        @toybot.state.should == 'inactive'
+      end
+
+      it 'should not change the coordinates' do
+        @toybot.pos.should == [nil, nil]
+      end
+
+      it 'should not change the direction' do
+        @toybot.dir.should be_nil
+      end
+    end
+
+    describe 'arguments are invalid when' do
+
+      it 'new position is outside the board' do
+        bot = docmd(%w{100 -5 north})
+        [bot.posx, bot.posy].should == [nil, nil]
+      end
+    end
+
   end
 
 end
